7278d78089fb57f9b11159e1a19e3ad3e411162e
Viktor Babchanik <vbabchanik@ucdavis.edu>
2016-06-24 01:19:22 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -6,11 +6,17 @@ int main() {
 	printf("1. Start a New Same\n2. View High Score\n3. Quit Game\n");
 	
 	char input[20] = "";
+	char* end;
 	int option = atoi(input);
-
+	
 	while((option != 1 && option != 2 && option != 3)) {
 		fgets(input, 20, stdin);
-		option = atoi(input);
+		option = strtol(input, &end, 10);
+		
+		if (end[0] != '\n') {
+			option = atoi(end);
+		}
+		
 		if (option != 1 && option != 2 && option != 3) {
 			printf("This option is not valid, please try again\n");
 		}
@@ -26,3 +32,27 @@ int main() {
 
 	return 0;
 } 
+/*
+typedef struct coordinates {
+	//a coordinate
+} coordinates;
+
+struct randomize_column_and_row() {
+	return coordinates;
+	//generates random coordinates
+}
+
+int two_or_four() {
+	return either a 2 or 4 randomly to start the game
+}
+
+char* generate_first_tile() {
+}
+
+char * generate_next_tile() {
+
+}
+
+void print_next_tiles() {
+
+}*/
